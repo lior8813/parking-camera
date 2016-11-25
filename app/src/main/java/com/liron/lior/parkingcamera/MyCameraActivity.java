856b2e67cf29c4a7f7248de47f9b913ee93deea9
lior <liormeduel@gmail.com>
2016-11-25 15:47:49 +0200

    initial commit

---
+++ app/src/main/java/com/liron/lior/parkingcamera/MyCameraActivity.java
@@ -0,0 +1,46 @@
+package com.liron.lior.parkingcamera;
+
+/**
+ * Created by lior on 11/19/2016.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MyCameraActivity extends Activity {
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.imageView = (ImageView)this.findViewById(R.id.image);
+        Button photoButton = (Button) this.findViewById(R.id.default_activity_button);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    private void startCamera(){
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+}
